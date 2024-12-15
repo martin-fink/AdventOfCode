086d36ffe3837ab620d4af5ca88f7dc621cc6659
Martin Fink <martin@finkmartin.com>
2024-12-15 22:21:07 +0100

    update day 15

--- 2024/day15/Main.hs
+++ 2024/day15/Main.hs
@@ -16,7 +16,11 @@ widenMat = concatMap widen
     widen c = [c, c]
 
 findStart :: Matrix Char -> (Int, Int)
-findStart m = head $ filter (\(x, y) -> getElem y x m == '@') [(x, y) | x <- [1 .. ncols m], y <- [1 .. nrows m]]
+findStart m =
+  head
+    $ filter
+        (\(x, y) -> getElem y x m == '@')
+        [(x, y) | x <- [1 .. ncols m], y <- [1 .. nrows m]]
 
 nextPos :: Char -> (Int, Int) -> (Int, Int)
 nextPos '>' (x, y) = (x + 1, y)
@@ -33,51 +37,51 @@ isUpDown _ = False
 moveWide :: Matrix Char -> (Int, Int) -> Char -> Maybe (Matrix Char)
 moveWide m p@(x, y) d =
   let p'@(x', y') = nextPos d p
-   in case (getElem y x m, isUpDown d) of
-        ('.', _) -> Just m
-        ('#', _) -> Nothing
-        (c, False) -> case moveWide m p' d of
-          Just m' ->
-            let m'' = setElem (getElem y' x' m') (y, x) m'
-             in Just $ setElem c (y', x') m''
-          Nothing -> Nothing
-        (c, True) ->
+   in case getElem y x m of
+        '.' -> Just m
+        '#' -> Nothing
+        c ->
           moveWide m p' d >>= \m' ->
             moveWide m' alternatePos d >>= \m'' ->
               let m''' = setElem '.' (y, x) m''
                   m'''' = setElem c (y', x') m'''
                in Just m''''
   where
-    alternatePos =
-      let p'@(x', y') = nextPos d p
-          c = getElem y' x' m
-       in case c of
-            '[' -> (x' + 1, y')
-            ']' -> (x' - 1, y')
-            _ -> p'
+    alternatePos
+      | isUpDown d =
+        let p'@(x', y') = nextPos d p
+            c = getElem y' x' m
+         in case c of
+              '[' -> (x' + 1, y')
+              ']' -> (x' - 1, y')
+              _ -> p'
+      | otherwise = nextPos d p
 
 moveRobotWide :: Matrix Char -> (Int, Int) -> Char -> (Matrix Char, (Int, Int))
-moveRobotWide m p d = case moveWide m p d of
-  Nothing -> (m, p)
-  Just m' -> (m', nextPos d p)
+moveRobotWide m p d =
+  case moveWide m p d of
+    Nothing -> (m, p)
+    Just m' -> (m', nextPos d p)
 
 getMovePos :: Matrix Char -> (Int, Int) -> Char -> Maybe (Int, Int)
-getMovePos m p@(x, y) dir = case getElem y x m of
-  '.' -> Just p
-  '#' -> Nothing
-  'O' -> getMovePos m (nextPos dir p) dir
-  _ -> error "invalid item"
+getMovePos m p@(x, y) dir =
+  case getElem y x m of
+    '.' -> Just p
+    '#' -> Nothing
+    'O' -> getMovePos m (nextPos dir p) dir
+    _ -> error "invalid item"
 
 moveRobot :: Matrix Char -> (Int, Int) -> Char -> (Matrix Char, (Int, Int))
-moveRobot m p@(x, y) dir = case getMovePos m (nextPos dir p) dir of
-  Nothing -> (m, p)
-  Just (x', y') ->
-    let p'@(x'', y'') = nextPos dir p
-        c = getElem y'' x'' m
-        m' = setElem c (y', x') m
-        m'' = setElem '@' (y'', x'') m'
-        m''' = setElem '.' (y, x) m''
-     in (m''', p')
+moveRobot m p@(x, y) dir =
+  case getMovePos m (nextPos dir p) dir of
+    Nothing -> (m, p)
+    Just (x', y') ->
+      let p'@(x'', y'') = nextPos dir p
+          c = getElem y'' x'' m
+          m' = setElem c (y', x') m
+          m'' = setElem '@' (y'', x'') m'
+          m''' = setElem '.' (y, x) m''
+       in (m''', p')
 
 parseInput :: (String -> String) -> String -> (Matrix Char, [Char])
 parseInput f s =
@@ -85,7 +89,12 @@ parseInput f s =
    in (parseMat f lhs, filter (/= '\n') rhs)
 
 calcScore :: Matrix Char -> Int
-calcScore m = sum [factor (getElem y x m) * (100 * (y - 1) + (x - 1)) | x <- [1 .. ncols m], y <- [1 .. nrows m]]
+calcScore m =
+  sum
+    [ factor (getElem y x m) * (100 * (y - 1) + (x - 1))
+    | x <- [1 .. ncols m]
+    , y <- [1 .. nrows m]
+    ]
   where
     factor 'O' = 1
     factor '[' = 1
@@ -94,20 +103,22 @@ calcScore m = sum [factor (getElem y x m) * (100 * (y - 1) + (x - 1)) | x <- [1 
 solve1 :: String -> Int
 solve1 s =
   let (m, moves) = parseInput id s
-   in calcScore . fst $ foldl (\(m', p) dir -> moveRobot m' p dir) (m, findStart m) moves
+   in calcScore . fst
+        $ foldl (\(m', p) dir -> moveRobot m' p dir) (m, findStart m) moves
 
 solve2 :: String -> Int
 solve2 s =
   let (m, moves) = parseInput widenMat s
-   in calcScore . fst $ foldl (\(m', p) dir -> moveRobotWide m' p dir) (m, findStart m) moves
+   in calcScore . fst
+        $ foldl (\(m', p) dir -> moveRobotWide m' p dir) (m, findStart m) moves
 
 tasks :: [(String -> Int, FilePath)]
 tasks =
-  [ (solve1, "day15/example.txt"),
-    (solve1, "day15/input.txt"),
-    (solve2, "day15/ex2.txt"),
-    (solve2, "day15/ex3.txt"),
-    (solve2, "day15/input.txt")
+  [ (solve1, "day15/example.txt")
+  , (solve1, "day15/input.txt")
+  , (solve2, "day15/ex2.txt")
+  , (solve2, "day15/ex3.txt")
+  , (solve2, "day15/input.txt")
   ]
 
 main :: IO ()
